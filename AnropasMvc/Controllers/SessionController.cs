6850d7fdc13b29b5a00924f9602dcd8e2ee92e5f
Emir Kocakaya <celalinko@gmail.com>
2025-03-19 17:18:15 +0300

    seans controller açıldı ve indexler hazırlandı

---
+++ AnropasMvc/Controllers/SessionController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AnropasMvc.Controllers
+{
+    public class SessionController : Controller
+    {
+        
+        public ActionResult AdminSessionIndex()
+        {
+            return View();
+        }
+
+        public ActionResult MemberSessionIndex()
+        {
+            return View();
+        }
+    }
+}
